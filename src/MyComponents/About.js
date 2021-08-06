c6b27c8c32ab0dcad16ef627827418ec406ec356
Shivam Pandya <shivam.pandya67@gmail.com>
2021-08-06 19:35:51 +0530

    my project

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+                This is an about component!!
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat 
+                    consequuntur odio ducimus esse excepturi voluptate dolorem veniam 
+                    ea optio iure repudiandae ullam quibusdam modi dolorum, enim magnam
+                    ipsa quas provident!
+                </p>
+        </div>
+    )
+}
